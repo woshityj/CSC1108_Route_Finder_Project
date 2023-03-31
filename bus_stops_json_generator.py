c4924c6f57fca9c936a21c2f4c93b6eeac2dad71
woshityj <yujietan84@gmail.com>
2023-04-01 01:30:39 +0800

    Fixed Marker

--- bus_stops_json_generator.py
+++ bus_stops_json_generator.py
@@ -2,21 +2,21 @@ import pandas as pd
 import pprint
 import json
 
-bus_stops = {}
-xls = pd.ExcelFile(r'bus_stops_cleaned_revised.xlsx')
-for sheets in xls.sheet_names:
-    data = pd.read_excel(r'bus_stops_cleaned_revised.xlsx', sheet_name = sheets)
-    df = pd.DataFrame(data)
-    bus_stops[sheets] = df.to_dict()
+# bus_stops = {}
+# xls = pd.ExcelFile(r'bus_stops_cleaned_revised.xlsx')
+# for sheets in xls.sheet_names:
+#     data = pd.read_excel(r'bus_stops_cleaned_revised.xlsx', sheet_name = sheets)
+#     df = pd.DataFrame(data)
+#     bus_stops[sheets] = df.to_dict()
 
-bus_stops_json = json.dumps(bus_stops, indent = 8)
-with open('bus_stops_cleaned_revised.json', 'w') as outfile:
-    outfile.write(bus_stops_json)
+# bus_stops_json = json.dumps(bus_stops, indent = 8)
+# with open('bus_stops_cleaned_revised.json', 'w') as outfile:
+#     outfile.write(bus_stops_json)
 
 # bus_stops = {}
-# xls = pd.ExcelFile(r'bus_stops_cleaned.xlsx')
+# xls = pd.ExcelFile(r'bus_stops_cleaned_revised.xlsx')
 # for sheets in xls.sheet_names:
-#     data = pd.read_excel(r'bus_stops_cleaned.xlsx', sheet_name = sheets)
+#     data = pd.read_excel(r'bus_stops_cleaned_revised.xlsx', sheet_name = sheets)
 #     df = pd.DataFrame(data)
 #     bus_stops.update((zip(df['Bus stop'], df['GPS Location'])))
 
